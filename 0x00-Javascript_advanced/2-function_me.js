e065bdd3b1ade8b592beb8a59c92a0358fe8ebb7
Esoteric918 <3411@holbertonschool.com>
2022-04-12 11:38:52 -0500

    closure task 2

---
+++ 0x00-Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+/**
+ * Closure
+ *
+ */
+
+function welcomMessage (fullName) {
+  alert(`Welcome ${fullName}`);
+}
+  const guillaume = () => welcomMessage('Guillaume');
+  const alex = () => welcomMessage('Alex');
+  const fred = () => welcomMessage('Fred');
