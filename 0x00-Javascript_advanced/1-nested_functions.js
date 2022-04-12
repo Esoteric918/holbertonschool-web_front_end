d80d9b4f6eb473e1946001da943046a442a953b6
Esoteric918 <3411@holbertonschool.com>
2022-04-12 10:47:26 -0500

    task 0 -1

---
+++ 0x00-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,23 @@
+/**
+ * closure scope chain
+ *
+ *
+ */
+const globalVariable = Welcome;
+
+function outer() {
+  alert(`${globalVariable}`);
+  const course = Holberton;
+
+  const inner = () => {
+    alert(`${globalVariable} ${course}`);
+    const exclamation = "!";
+
+  const inception = () => {
+    alert(`${globalVariable} ${course} ${exclamation}`);
+    inception();
+    };
+   inner();
+  };
+  outer();
+};
