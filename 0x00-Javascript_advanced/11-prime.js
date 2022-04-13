b68876461061d0a8533edeb5000a014567c4d2d5
Esoteric918 <3411@holbertonschool.com>
2022-04-13 11:13:07 -0500

    task 11

---
+++ 0x00-Javascript_advanced/11-prime.js
@@ -0,0 +1,23 @@
+setTimeout(() => {console.log(`Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`)});
+
+function countPrimeNumbers() {
+  let prime = [];
+  for (let i = 2; i <= 100; i++) {
+  var not_prime = false;
+  for (let div = 3; div <= i ; div+=2 ) {
+      if ( div % i === 0 ) {
+        not_prime = true;
+        }
+      }
+      if (not_prime === false) {
+          prime.push(i);
+        }
+      }
+      return prime;
+}
+
+const t0 = performance.now();
+ for (let x = 1; x <= 100; ++x){
+  setTimeout(() =>countPrimeNumbers());
+}
+const t1 = performance.now();
